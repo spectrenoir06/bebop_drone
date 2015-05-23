4b00b18c9221add2fdeb7dc2040e2e2f28d47b15
spectrenoir06 <antoine@doussaud.org>
2015-05-23 23:40:18 +0100

    love left right front

--- main.lua
+++ main.lua
@@ -25,7 +25,7 @@ function bebop.initialize()
    local o = {
        x        = 1280/2,
        y        = 720/2,
-       front    = 50,
+       front    = 0,
        left     = 0,
        right    = 0,
        bottom   = 0,
@@ -35,9 +35,8 @@ function bebop.initialize()
    return new._object(bebop, o)
 end
 
-function bebop:draw()
-    love.graphics.setColor( 255, 255, 255, 255)
-    love.graphics.draw( self.img,self.x, self.y,  math.rad(90), 1, 1, self.img:getWidth() / 2, self.img:getHeight() / 2)
+
+function bebop:drawFront()
     if self.front < 75 then
         love.graphics.setColor( 255, 0, 0, 255)
     else
@@ -45,6 +44,37 @@ function bebop:draw()
     end
     love.graphics.print( math.ceil(self.front).." cm", 1280/2 - 200, 720/2 - 50)
     love.graphics.rectangle( 'fill', 1280/2 - self.img:getWidth() / 2 - self.front + 100, 720/2 - self.largeur / 2 , self.front, self.largeur)
+end
+
+
+function bebop:drawLeft()
+    if self.left < 75 then
+        love.graphics.setColor( 255, 0, 0, 255)
+    else
+        love.graphics.setColor( 0, 255, 0, 255)
+    end
+    love.graphics.print( math.ceil(self.left).." cm", 1280/2 - 30, 720/2 + 70)
+    love.graphics.rectangle( 'fill', 1280/2 - self.largeur / 2, 720/4 * 2.5, self.largeur, self.left)
+end
+
+function bebop:drawRight()
+    if self.left < 75 then
+        love.graphics.setColor( 255, 0, 0, 255)
+    else
+        love.graphics.setColor( 0, 255, 0, 255)
+    end
+    love.graphics.print( math.ceil(self.right).." cm", 1280/2 - 30, 720/2 - 70)
+    love.graphics.rectangle( 'fill', 1280/2 - self.largeur / 2, 720/4 * 1.5 - self.right, self.largeur, self.right)
+end
+
+
+function bebop:draw()
+    love.graphics.setColor( 255, 255, 255, 255)
+    love.graphics.draw( self.img,self.x, self.y,  math.rad(90), 1, 1, self.img:getWidth() / 2, self.img:getHeight() / 2)
+
+    self:drawFront()
+    self:drawLeft()
+    self:drawRight()
 end
 
 function bebop:send(roll, pitch, yaw, gaz)
@@ -116,8 +146,14 @@ j = 0
 
 function love.update(dt)
     j = j + dt
-
-    drone.front = rserial:read('*n')
+    str = rserial:read()
+    if str then
+        print(str)
+        drone.left, drone.right = string.match(str,"(%d+) (%d+)")
+        drone.left = tonumber(drone.left)
+        drone.right = tonumber(drone.right)
+        print(drone.left, drone.right)
+    end
     rserial:flush()
 
     if j > 0.02 then
