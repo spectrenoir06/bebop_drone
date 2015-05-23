f83f0c488640e77c5ade208adeab1c3faee7cdf8
spectrenoir06 <antoine@doussaud.org>
2015-05-23 23:14:05 +0100

    left right

--- ultrason/ultrason.ino
+++ ultrason/ultrason.ino
@@ -1,23 +1,50 @@
-int trig = 1; 
-int echo = 2; 
+int right_trig = 1; 
+int right_echo = 2;
+
+int left_trig = 19; 
+int left_echo = 20; 
+
 long lecture_echo; 
 long cm;
 
 void setup() 
 { 
-  pinMode(trig, OUTPUT); 
-  digitalWrite(trig, LOW); 
-  pinMode(echo, INPUT); 
+  pinMode(left_trig, OUTPUT);
+  pinMode(right_trig, OUTPUT);
+  
+  digitalWrite(left_trig, LOW);
+  digitalWrite(right_trig, LOW); 
+  
+  pinMode(left_echo, INPUT); 
+  pinMode(right_echo, INPUT); 
+  
   Serial.begin(9600); 
 }
 
-void loop() 
-{ 
-  digitalWrite(trig, HIGH); 
+long scanLeft()
+{
+  digitalWrite(left_trig, HIGH); 
   delayMicroseconds(10); 
-  digitalWrite(trig, LOW); 
-  lecture_echo = pulseIn(echo, HIGH); 
-  cm = lecture_echo / 58; 
-  Serial.println(cm); 
+  digitalWrite(left_trig, LOW); 
+  lecture_echo = pulseIn(left_echo, HIGH); 
+  return (lecture_echo / 58); 
+}
+
+long scanRight()
+{
+  digitalWrite(right_trig, HIGH); 
+  delayMicroseconds(10); 
+  digitalWrite(right_trig, LOW); 
+  lecture_echo = pulseIn(right_echo, HIGH); 
+  return (lecture_echo / 58); 
+}
+
+
+
+void loop() 
+{
+  Serial.print(scanLeft());
+  Serial.print(" ");
+ Serial.println(scanRight()); 
   delay(100); 
 }
