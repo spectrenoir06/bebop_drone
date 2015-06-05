d12eab0a8b16b2af79784be3f723b61965665798
Antoine DOUSSAUD <adoussau@e3r1p3.42.fr>
2015-06-05 18:33:06 +0200

    strcut

--- wifi/wifi.ino
+++ wifi/wifi.ino
@@ -3,11 +3,12 @@
 #define rxPin 13
 #define txPin 14
 
+/*
 #define frame_type      0x02
 
 #define buffer_id       0x0a
+*/
 
-#define id_project      0x01
 #define class_piloting  0x00
 #define cmd_flatTrim    0x00
 #define cmd_takeoff     0x01
@@ -15,8 +16,8 @@
 #define cmd_land        0x03
 #define cmd_emergency   0x04
 
-#define flag             0x00
-
+#define id_project       0x01
+/*
 uint8_t packet[13] = {
   0x03,
   0x0b,
@@ -25,11 +26,9 @@ uint8_t packet[13] = {
   id_project,
   class_piloting,
   cmd_takeoff,
-  0x00,
-  '\r',
-  '\n'
+  0x00
 };
-
+*/
 uint8_t packet2[22] = {
   0x02,
   0x0a,
@@ -39,7 +38,7 @@ uint8_t packet2[22] = {
   class_piloting,
   cmd_pcmd,
   0x00,
-  flag,
+  0,
   0x00, // roll
   0x9c, // pitch
   0x00, // yaw
@@ -47,9 +46,7 @@ uint8_t packet2[22] = {
   0x00,
   0x00,
   0x00,
-  0x00,
-  '\r',
-  '\n'
+  0x00
 };
 
 uint8_t packet3[22] = {
@@ -61,7 +58,7 @@ uint8_t packet3[22] = {
   class_piloting,
   cmd_pcmd,
   0x00,
-  flag,
+  0,
   0x00, // roll
   0x9c, // pitch
   0x00, // yaw
@@ -69,9 +66,47 @@ uint8_t packet3[22] = {
   0x00,
   0x00,
   0x00,
-  0x00,
-  '\r',
-  '\n'
+  0x00
+};
+
+typedef struct s_packet
+{
+  uint8_t  frame_type;
+  uint8_t  buffer_id;
+  uint8_t  sequence;
+  uint32_t size;
+  uint8_t  id;
+  uint8_t  class_type;
+  uint16_t cmd;
+}              t_packet;
+
+typedef struct s_pcmd
+{
+  uint8_t  flag;
+  int8_t  roll;
+  int8_t  pitch;
+  int8_t  yaw;
+  int8_t  gaz;
+  float   psi;
+}              t_arg_pcmd;
+
+t_packet packet = {
+  0x03,
+  0x0b,
+  0x42,
+  20,
+  id_project,
+  class_piloting,
+  cmd_takeoff,
+};
+
+t_arg_pcmd packet_pcmd_arg = {
+  0,     // flag   Boolean flag to activate roll/pitch movement
+  0,     // roll   Roll consign for the drone  [-100;100]
+  -100,  // pitch  Pitch consign for the drone [-100;100]
+  0,     // yaw    Yaw consign for the drone   [-100;100]
+  0,     // gaz    Gaz consign for the drone   [-100;100]
+  0,     // psi [NOT USED] - Magnetic north heading of the controlling device (deg) [-180;180]
 };
 
 SoftwareSerial mySerial =  SoftwareSerial(rxPin, txPin);
@@ -90,7 +125,10 @@ void setup()
   delay(8000);
   while (mySerial.available())
     Serial.write(mySerial.read());
-  mySerial.println("AT+CWJAP=\"BebopDrone-B030867\",\"\"\r");
+  
+  //mySerial.println("AT+CWJAP=\"BebopDrone-B030867\",\"\"\r");
+  mySerial.println("AT+CWJAP=\"Spectre\",\"\"\r");
+  
   delay(5000);
   while (mySerial.available())
     Serial.write(mySerial.read());
@@ -98,7 +136,7 @@ void setup()
   delay(3000);
   while (mySerial.available())
     Serial.write(mySerial.read());
-  mySerial.println("AT+CIPSTART=4,\"UDP\",\"192.168.42.1\",54321\r");
+  mySerial.println("AT+CIPSTART=4,\"UDP\",\"192.168.43.14\",54321\r");
   delay(3000);
   while (mySerial.available())
     Serial.write(mySerial.read());
@@ -107,7 +145,7 @@ void setup()
   mySerial.println("AT+CIPSEND=4,13\r");
   delay(1000);
   
-  mySerial.write(packet,13);
+  mySerial.write((byte*)&packet, 13);
    while (mySerial.available())
     Serial.write(mySerial.read());
   delay(1000);
@@ -117,16 +155,18 @@ void setup()
 
 void loop() // run over and over
 {
-  /*if (mySerial.available())
-    Serial.write(mySerial.read());
-  if (Serial.available())
-    mySerial.write(Serial.read());
-    */
+    mySerial.println("AT+CIPSEND=4,20\r");
+    mySerial.write(packet3,20);
+    delay(500);
     
-    mySerial.println("AT+CIPSEND=4,22\r");
-    mySerial.write(packet3,22);
-    mySerial.println("AT+CIPSEND=4,22\r");
-    mySerial.write(packet2,22);
-    delay(50);
+    while (mySerial.available())
+    Serial.write(mySerial.read());
+    
+    mySerial.println("AT+CIPSEND=4,20\r");
+    /*mySerial.write(packet2,20);
+    delay(500);
+    
+    while (mySerial.available())
+    Serial.write(mySerial.read());*/
     
 }
