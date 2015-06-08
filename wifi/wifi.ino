aa99b4a00db473d4683976f69fcda2edd59ec169
Vallium <aalliot@student.42.fr>
2015-06-08 13:14:16 +0200

    menage

--- wifi/wifi.ino
+++ wifi/wifi.ino
@@ -1,13 +1,3 @@
-//#include <SoftwareSerial.h>
-
-#define rxPin 13
-#define txPin 14
-
-/*
-#define frame_type      0x02
-
-#define buffer_id       0x0a
-*/
 
 #define class_piloting  0x00
 #define cmd_flatTrim    0x00
@@ -17,60 +7,6 @@
 #define cmd_emergency   0x04
 
 #define id_project       0x01
-/*
-uint8_t packet[13] = {
-  0x03,
-  0x0b,
-  0x56,
-  0x0b, 0x00, 0x00, 0x00,
-  id_project,
-  class_piloting,
-  cmd_takeoff,
-  0x00
-};
-*/
-
-/*
-uint8_t packet2[22] = {
-  0x02,
-  0x0a,
-  32,
-  0x14, 0x00, 0x00, 0x00,
-  id_project,
-  class_piloting,
-  cmd_pcmd,
-  0x00,
-  0,
-  0x00, // roll
-  0x9c, // pitch
-  0x00, // yaw
-  0x00,
-  0x00,
-  0x00,
-  0x00,
-  0x00
-};
-
-uint8_t packet3[22] = {
-  0x02,
-  0x0a,
-  42,
-  0x14, 0x00, 0x00, 0x00,
-  id_project,
-  class_piloting,
-  cmd_pcmd,
-  0x00,
-  0,
-  0x00, // roll
-  0x9c, // pitch
-  0x00, // yaw
-  0x00,
-  0x00,
-  0x00,
-  0x00,
-  0x00
-};
-*/
 
 typedef struct s_packet
 {
@@ -112,56 +48,44 @@ t_arg_pcmd packet_pcmd_arg = {
   0,     // psi [NOT USED] - Magnetic north heading of the controlling device (deg) [-180;180]
 };
 
-//SoftwareSerial mySerial =  SoftwareSerial(rxPin, txPin);
-
-void setup()  
+void setup()
 {
   // Open serial communications and wait for port to open:
   Serial.begin(115200);
   Serial1.begin(115200);
-  
-  Serial.println("Start");
-  //pinMode(rxPin, INPUT);
-  //pinMode(txPin, OUTPUT);
 
-  // set the data rate for the SoftwareSerial port
-  //Serial.begin(9600);
+  Serial.println("Start");
+
   Serial1.println("AT+RST\r");
-  Serial.println("hello");
-  
+
   while (Serial1.find("ready"));
-  
+
 //  Serial1.println("AT+CWJAP=\"BebopDrone-B030867\",\"\"\r");
   Serial1.println("AT+CWJAP=\"Spectre\",\"\"\r");
-  
+
   while (Serial1.find("OK"));
-  
-  
-  while (Serial1.available())
-    Serial.write(Serial1.read());
-  Serial.println();
-  
+
   Serial1.println("AT+CIPMUX=1\r");
-  
+
   while (Serial1.find("OK"));
-  
-  
+
+
 //  Serial1.println("AT+CIPSTART=4,\"UDP\",\"192.168.42.1\",54321\r");
   Serial1.println("AT+CIPSTART=4,\"UDP\",\"192.168.43.14\",54321\r");
-  
+
   while (Serial1.find("OK"));
- 
+
   Serial1.println("AT+CIPSEND=4,11\r");
   while (Serial1.find(">"));
   Serial1.write((byte*)&packet, sizeof(packet));
   //Serial1.write('\r');
   //Serial1.write('\n');
-  
+
   delay(5000);
-  
+
   while (Serial1.available())
     Serial.write(Serial1.read());
-  
+
   packet.frame_type = 0x02;
   packet.buffer_id  = 0x0a;
   packet.size       = 20;
@@ -172,29 +96,21 @@ void loop() // run over and over
 {
     for (int i = 0; i < 50; i++)
     {
-      
-      packet.sequence = i%50; 
-      
-      Serial1.println("AT+CIPSEND=4,20\r");   // send on channel 4 22byte  
-      
-      //delay(100);
-      while (!Serial1.find(">"));             // wait         
-      
+      packet.sequence = i%50;
+
+      Serial1.println("AT+CIPSEND=4,20\r");   // send on channel 4 20byte
+
+      while (!Serial1.find(">"));             // wait
+
       Serial1.write((byte*)&packet, sizeof(packet));                   // write 11byte
       Serial1.write((byte*)&packet_pcmd_arg, sizeof(packet_pcmd_arg)); // write 9byte
       //Serial1.write('\r');
       //Serial1.write('\n');
-      
+
       while (!Serial1.find("K")); // wait
-      //delay(200);
-       
     }
-    
-     while (Serial.available())
-     Serial.write(Serial.read());
-    
-    while (42) 
+    while (42)
     {
     }
-    
+
 }
